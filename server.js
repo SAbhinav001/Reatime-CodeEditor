93af9db3c5b009297c8694b92e3ccf3798df3dab
SAbhinav001 <abhinav.temp01@gmail.com>
2023-10-18 23:15:21 +0530

    refactoring and sync code

--- server.js
+++ server.js
@@ -8,11 +8,55 @@ const PORT = process.env.PORT || 3017;
 
 const io = new Server(server);
 
+const userSocketmap = {};
+
+function getAllConnectedClients(roomId) {
+  // Map
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketmap[socketId],
+      };
+    }
+  );
+}
+
 io.on("connection", (socket) => {
   console.log("socket cnnected", socket.id);
 
   socket.on("join", ({ roomId, username }) => {
     console.log(roomId, username);
+    userSocketmap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on("code-change", ({ roomId, code }) => {
+    socket.in(roomId).emit("code-change", { code });
+  });
+
+  socket.on("sync-code", ({ socketId, code }) => {
+    io.to(socketId).emit("sync-code", { code });
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketmap[socket.id],
+      });
+    });
+    delete userSocketmap[socket.id];
+    socket.leave();
   });
 });
 
--- src/pages/Editorpage.jsx
+++ src/pages/Editorpage.jsx
@@ -1,72 +1,123 @@
 import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
+import ACTIONS from "../Actions";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
 import { initSocket } from "../socket";
-import ACTIONS from "../Actions";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import toast from "react-hot-toast";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 
-function Editorpage() {
+const EditorPage = () => {
   const socketRef = useRef(null);
-  const { roomId } = useParams();
-  const navigate = useNavigate();
+  const codeRef = useRef(null);
   const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
-
       socketRef.current.on("connect_error", (err) => handleErrors(err));
       socketRef.current.on("connect_failed", (err) => handleErrors(err));
 
       function handleErrors(e) {
         console.log("socket error", e);
-        toast.error("Socket connection failed, try again later");
-        navigate("/");
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
       }
 
       socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
         username: location.state?.username,
       });
+
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+
+      // Listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
     };
     init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
   }, []);
 
-  const [clients, setClient] = useState([
-    {
-      socketId: 1,
-      username: "abhinav",
-    },
-    {
-      socketId: 2,
-      username: "shrivastav",
-    },
-  ]);
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success("Room ID has been copied to your clipboard");
+    } catch (err) {
+      toast.error("Could not copy the Room ID");
+      console.error(err);
+    }
+  }
+
+  function leaveRoom() {
+    reactNavigator("/");
+  }
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className="mainWrap">
       <div className="aside">
         <div className="asideInner">
           <div className="logo">
-            <img className="logoimg" src="/code-sync.png" alt="Logoimg" />
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
           </div>
           <h3>Connected</h3>
-          <div className="clientList">
-            {clients.map((client) => {
-              return (
-                <Client key={client.socketId} username={client.username} />
-              );
-            })}
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
           </div>
         </div>
-        <button className="btn copybtn">Copy Room ID</button>
-        <button className="btn leavebtn">Leave</button>
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
       </div>
       <div className="editorWrap">
-        <Editor />
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
       </div>
     </div>
   );
-}
+};
 
-export default Editorpage;
+export default EditorPage;
