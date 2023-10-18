4eafe062640797dd47cdf8a15673d26a01c059fd
SAbhinav001 <abhinav.temp01@gmail.com>
2023-10-18 17:03:37 +0530

    Server config

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 const app = express();
-const http = require("http");
 
-const { Server } = require("socket.io");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3017;
 
@@ -10,7 +10,13 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("socket cnnected", socket.id);
+
+  socket.on("join", ({ roomId, username }) => {
+    console.log(roomId, username);
+  });
 });
+
+app.use("/", () => console.log("stared"));
 
 server.listen(PORT, () => {
   console.log("sever start at", PORT);
--- src/pages/Editorpage.jsx
+++ src/pages/Editorpage.jsx
@@ -1,8 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import ACTIONS from "../Actions";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function Editorpage() {
+  const socketRef = useRef(null);
+  const { roomId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later");
+        navigate("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+    };
+    init();
+  }, []);
+
   const [clients, setClient] = useState([
     {
       socketId: 1,
